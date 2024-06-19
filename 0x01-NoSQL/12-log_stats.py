8db5139dd7fd6fea85746922fa0c9c35bd04d8bd
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-06-19 03:35:18 +0300

    MySQL advanced

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -17,14 +17,3 @@ def print_nginx_request_logs(nginx_collection):
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
     print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
-
-
-if __name__ == '__main__':
-    run()
